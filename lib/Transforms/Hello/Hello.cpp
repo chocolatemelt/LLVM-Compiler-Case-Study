b0576f6e1e386d5e6afe7a2cbf76a288cb63971c
Kevin Zhang <kzhaaang@gmail.com>
2016-10-03 21:39:06 -0400

    update hello.cpp for hw1

--- lib/Transforms/Hello/Hello.cpp
+++ lib/Transforms/Hello/Hello.cpp
@@ -14,52 +14,66 @@
 
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/InstVisitor.h"
+#include "llvm/IR/Instruction.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+
 using namespace llvm;
 
 #define DEBUG_TYPE "hello"
 
-STATISTIC(HelloCounter, "Counts number of functions greeted");
-
 namespace {
-  // Hello - The first implementation, without getAnalysisUsage.
+  struct BinaryInverter:
+		public InstVisitor<BinaryInverter> {
+			void visitBinaryOperator(BinaryOperator &bo) {
+				auto inverted = Instruction::Add;
+				switch(bo.getOpcode()) {
+					case Instruction::Add:
+						inverted = Instruction::Sub;
+						break;
+					case Instruction::Sub:
+						inverted = Instruction::Add;
+						break;
+					case Instruction::Mul:
+						inverted = Instruction::UDiv;
+						break;
+					case Instruction::UDiv: case Instruction::SDiv:
+						inverted = Instruction::Mul;
+						break;
+					case Instruction::FAdd:
+						inverted = Instruction::FSub;
+						break;
+					case Instruction::FSub:
+						inverted = Instruction::FAdd;
+						break;
+					case Instruction::FMul:
+						inverted = Instruction::FDiv;
+						break;
+					case Instruction::FDiv:
+						inverted = Instruction::FMul;
+						break;
+					default: break;
+				}
+				auto binverted = BinaryOperator::Create(inverted, bo.getOperand(0), bo.getOperand(1));
+				ReplaceInstWithInst(&bo, binverted);
+				didChange = true;
+			}
+			bool didChange = false;
+		};
+
   struct Hello : public FunctionPass {
     static char ID; // Pass identification, replacement for typeid
     Hello() : FunctionPass(ID) {}
 
     bool runOnFunction(Function &F) override {
-      ++HelloCounter;
-      errs() << "Hello: ";
-      errs().write_escaped(F.getName()) << '\n';
-      return false;
+			BinaryInverter op;
+			op.visit(F);
+			return op.didChange;
     }
   };
 }
 
 char Hello::ID = 0;
 static RegisterPass<Hello> X("hello", "Hello World Pass");
-
-namespace {
-  // Hello2 - The second implementation with getAnalysisUsage implemented.
-  struct Hello2 : public FunctionPass {
-    static char ID; // Pass identification, replacement for typeid
-    Hello2() : FunctionPass(ID) {}
-
-    bool runOnFunction(Function &F) override {
-      ++HelloCounter;
-      errs() << "Hello: ";
-      errs().write_escaped(F.getName()) << '\n';
-      return false;
-    }
-
-    // We don't modify the program, so we preserve all analyses.
-    void getAnalysisUsage(AnalysisUsage &AU) const override {
-      AU.setPreservesAll();
-    }
-  };
-}
-
-char Hello2::ID = 0;
-static RegisterPass<Hello2>
-Y("hello2", "Hello World Pass (with getAnalysisUsage implemented)");
