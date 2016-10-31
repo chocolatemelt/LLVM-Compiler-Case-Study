889542bfdd1cbf3c06481bbf8852b808e8a84379
Kevin Zhang <kzhaaang@gmail.com>
2016-10-30 21:14:47 -0400

    whatever i have now is whatever i have now. que sera, sera

--- lib/Transforms/Hello/Hello.cpp
+++ lib/Transforms/Hello/Hello.cpp
@@ -11,68 +11,129 @@
 // in docs/WritingAnLLVMPass.html
 //
 //===----------------------------------------------------------------------===//
+#include <functional>
+#include <set>
+#include <stack>
 
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
 #include "llvm/IR/InstVisitor.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/Pass.h"
+#include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 
-using namespace llvm;
-
 #define DEBUG_TYPE "hello"
 
+using namespace llvm;
+
 namespace {
-  struct BinaryInverter:
-		public InstVisitor<BinaryInverter> {
-			void visitBinaryOperator(BinaryOperator &bo) {
-				auto inverted = Instruction::Add;
-				switch(bo.getOpcode()) {
-					case Instruction::Add:
-						inverted = Instruction::Sub;
-						break;
-					case Instruction::Sub:
-						inverted = Instruction::Add;
-						break;
-					case Instruction::Mul:
-						inverted = Instruction::UDiv;
-						break;
-					case Instruction::UDiv: case Instruction::SDiv:
-						inverted = Instruction::Mul;
-						break;
-					case Instruction::FAdd:
-						inverted = Instruction::FSub;
-						break;
-					case Instruction::FSub:
-						inverted = Instruction::FAdd;
-						break;
-					case Instruction::FMul:
-						inverted = Instruction::FDiv;
-						break;
-					case Instruction::FDiv:
-						inverted = Instruction::FMul;
-						break;
-					default: break;
+	template<typename T> T safe_pop(std::stack<T> &s) {
+		auto tmp = s.top();
+		s.pop();
+		return tmp;
+	}
+
+	/* handle globals */
+	int getGlobalValue(GlobalVariable *GV) {
+		return *GV->getInitializer()->getUniqueInteger().getRawData();
+	}
+
+	int getGlobalValue(LoadInst *LI, GlobalVariable *GV) {
+		static std::map<LoadInst*, int> loader;
+		if(loader.find(LI) == loader.end()) loader[LI] = getGlobalValue(GV);
+		return loader[LI];
+	}
+
+	void setGlobalValue(GlobalVariable *GV, int val) {
+		auto origConstant = dyn_cast<ConstantInt>(GV->getInitializer());
+		assert(origConstant != nullptr && "Global Variable wasn't an integer");
+		auto constant = ConstantInt::get(origConstant->getType(), val);
+		GV->setInitializer(constant);
+	}
+
+	/* create the function's inverse */
+	Function *constructInverseFunction(
+			Function &F,
+			LLVMContext &C,
+			std::stack<Expr *> &inverse,
+			std::stack<GlobalVariable *> &globals) {
+		// no args, foo_inverse
+		auto mod = F.getEntryBlock().getModule();
+		auto foo_inverse = cast<Function>(
+				mod->getOrInsertFunction("foo_inverse", Type::getVoidTy(C), NULL));
+		auto entry = BasicBlock::Create(foo_inverse->getContext(), "entry", foo_inverse);
+		IRBuilder<> builder(entry);
+
+		// build the IR in reverse order
+		while (!inverse.empty()) {
+			auto expr = safe_pop(inverse);
+			auto glob = safe_pop(globals);
+			ExprCodeGenVisitor CV(builder, foo_inverse->getContext());
+			expr->accept(&CV);
+			auto store = builder.CreateStore(CV.get_value(), glob);
+			DEBUG(errs() << *store << "\n");
+		}
+		auto ret = builder.CreateRetVoid();
+		DEBUG(errs() << *ret << "\n");
+
+		// replace foo's uses by foo_inverse
+		auto foo = cast<Function>(mod->getOrInsertFunction("foo", Type::getVoidTy(C), NULL));
+		foo->replaceAllUsesWith(foo_inverse);
+
+		return foo_inverse;
+	}
+
+	// Handles the reversal of the code
+	struct GlobalReverser :
+		public InstVisitor<GlobalReverser> {
+
+			std::stack<Expr *> inverse;
+			std::stack<GlobalVariable *> globalStores;
+
+			void visitStoreInst(StoreInst &SI) {
+				auto Op = SI.getPointerOperand();
+				if (GlobalVariable *GV = dyn_cast<GlobalVariable>(Op)) {
+					// Construct the Expression hierarchy and evaluate
+					Expr *expr = constructExpr(SI.getOperand(0));
+					ExprEvalVisitor EV;
+					expr->accept(&EV);
+					setGlobalValue(GV, EV.get_evaluation());
+					errs() << "... Evaluated " << GV->getName()
+						<< "=" << getGlobalValue(GV) << "\n";
+					// Reverse the operations and place it onto a stack to be
+					// transformed by the IRBuilder later.
+					GetBinaryOps GBO;
+					expr->accept(&GBO);
+					ExprReverseVisitor RV(std::move(GBO.s));
+					expr->accept(&RV);
+					inverse.push(RV.get_inv());
+					globalStores.push(GV);
 				}
-				auto binverted = BinaryOperator::Create(inverted, bo.getOperand(0), bo.getOperand(1));
-				ReplaceInstWithInst(&bo, binverted);
-				didChange = true;
 			}
-			bool didChange = false;
 		};
 
-  struct Hello : public FunctionPass {
-    static char ID; // Pass identification, replacement for typeid
-    Hello() : FunctionPass(ID) {}
+	// Driver, runs the reverser pass, and then constructs the inverse function
+	struct Hello : public FunctionPass {
+		static char ID;
+		Hello() : FunctionPass(ID) {}
 
-    bool runOnFunction(Function &F) override {
-			BinaryInverter op;
-			op.visit(F);
-			return op.didChange;
-    }
-  };
+		bool runOnFunction(Function &F) override {
+			if (F.getName() == "foo") {
+				errs() << "[+] Constructing expression tree\n";
+				GlobalReverser bodg;
+				bodg.visit(F);
+				errs() << "[+] Constructing function inverse\n";
+				auto inverse = constructInverseFunction(F, F.getContext(),
+						bodg.inverse, bodg.globalStores);
+				errs() << "[+] Inverted function [" << F.getName()
+					<< "] to [" << inverse->getName() << "]\n";
+				return true;
+			}
+			return false;
+		}
+	};
 }
 
 char Hello::ID = 0;
